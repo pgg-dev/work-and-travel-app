3e9271970399ac1ce47968904bf45731d005a18e
pgg-dev <park.geumgyu@gmail.com>
2022-04-07 06:14:07 +0900

    FEAT: submit 이벤트 추가

--- App.js
+++ App.js
@@ -12,9 +12,21 @@ import { theme } from "./colors";
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+    setToDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -39,7 +51,9 @@ export default function App() {
       </View>
       <View>
         <TextInput
+          onSubmitEditing={addToDo}
           onChangeText={onChangeText}
+          returnKeyType="done"
           value={text}
           placeholder={working ? "Add a To Do" : "Where do you want to go?"}
           style={styles.input}
